3764fe94f3b06c2f9f0731a92b9ec21a4eaddb21
siriiane <sirine.benghazala@lepoles.org>
2024-05-27 17:00:15 +0200

    27_05

---
+++ public/nav.html.php
@@ -0,0 +1,18 @@
+<nav>
+    <div class="linklist">
+        <ul>
+            <li><a href="">structure</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">selecteur</a></li>
+            <li><a href="">image</a></li>
+            <li><a href="">position</a></li>
+            <li><a href="">zoning</a></li>
+            <li><a href="">tableau</a></li>
+            <li><a href="">liste</a></li>
+            <li><a href="">liens & menu</a></li>
+            <li><a href="">formulaire</a></li>
+            <li><a href="">multimedia</a></li>
+            <li><a href="">animation</a></li>
+        </ul>
+    </div>
+</nav>
