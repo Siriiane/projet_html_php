1e6ad122a78bf7ea9a67b180c1b740ed9062bdca
siriiane <sirine.benghazala@lepoles.org>
2024-05-27 17:00:15 +0200

    28_05

---
+++ public/header.html.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="http://localhost/projet_html_php/">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+
+
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="public\assets\img\logo.png" alt="">
+        </div>
+        <div class="titre">
+            <h1>Mon premier site en html/css</h1>
+        </div>
+        <div class="social">
+            <a href=""><i class="fa-brands fa-square-facebook fa-2xl"></i></a>
+            <br>
+            <br>
+            <a href=""><i class="fa-brands fa-instagram fa-2xl"></i></a>
+        </div>
+    </header>
+
+    <?php require __DIR__ . "/nav.html.php" ?>
---
+++ zoning.php
@@ -0,0 +1,2 @@
+<?php require __DIR__ . "/public/header.html.php"?>
+
