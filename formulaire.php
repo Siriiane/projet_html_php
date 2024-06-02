571365a58f57dbba4430d0437b08cfdf02cd34fa
siriiana <sirine.benghazala@lespoles.org>
2024-06-02 17:33:46 +0200

    02_06

--- formulaire.php
+++ formulaire.php
@@ -2,6 +2,13 @@
 
 <main>
 <h1>Formulaire</h1>
-<form action=""></form>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
 </main>
 <?php require __DIR__ . "/public/footer.html.php"?>
